9c9f17132323f0667d0dd749adee19c480b255d2
prince475 <danielmwai475@gmail.com>
2023-03-19 00:17:03 +0300

    Completed Validations both at controller level and model level

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,27 @@
 class AuthorsController < ApplicationController
-  
-  def show
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+def show
     author = Author.find(params[:id])
 
     render json: author
-  end
+end
 
-  def create
-    author = Author.create(author_params)
+def create
+  author = Author.create!(author_params)
 
-    render json: author, status: :created
-  end
+  render json: author, status: :created
+end
 
   private
-  
-  def author_params
-    params.permit(:email, :name)
-  end
-  
+
+def author_params
+  params.permit(:email, :name)
+end
+
+def render_unprocessable_entity_response(invalid)
+  render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+end
+
 end
